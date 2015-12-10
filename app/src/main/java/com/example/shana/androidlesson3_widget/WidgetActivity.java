73a4913513ad27752bec246a4eff9dc64606fc85
Twilight <rie00002@gmail.com>
2015-12-10 17:02:35 +0800

    First page done

--- app/src/main/java/com/example/shana/androidlesson3_widget/WidgetActivity.java
+++ app/src/main/java/com/example/shana/androidlesson3_widget/WidgetActivity.java
@@ -1,12 +1,14 @@
 package com.example.shana.androidlesson3_widget;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 
 /**
  * Created by shana on 2015/12/10.
@@ -30,12 +32,24 @@ public class WidgetActivity extends Activity {
     @Bind(R.id.activity_widget_list_view)
     ListView listView;
 
+    @OnItemClick(R.id.activity_widget_list_view)
+    void onListViewItemClick(int position){
+        startActivity(new Intent(this, (Class)activities[position*2 + 1]));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widget);
         ButterKnife.bind(this);
-        CharSequence list = new CharSequence[];
-        listView.setAdapter(new ArrayAdapter<CharSequence>(this, android.R.simple_list_item_1, list));
+        listView.setAdapter(new ArrayAdapter<CharSequence>(this, android.R.layout.simple_list_item_1, getWidgetList()));
+    }
+
+    private CharSequence[] getWidgetList(){
+        CharSequence[] list = new CharSequence[activities.length/2];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = (String)activities[i*2];
+        }
+        return list;
     }
 }
